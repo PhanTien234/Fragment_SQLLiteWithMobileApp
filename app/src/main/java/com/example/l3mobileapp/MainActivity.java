3a0282dd97bc3fb381400259db85fa1de6985a2f
PhanTien234 <minhtienp328@gmail.com>
2023-07-21 11:14:12 +0700

    Initilize L3 Mobile App

---
+++ app/src/main/java/com/example/l3mobileapp/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.l3mobileapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+    // Create an array with options
+    private String [] workStatus = {"Employed", "Unemployed"};
+    Spinner sp;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Get reference to spinner
+        sp = findViewById(R.id.spinner);
+
+        // Create an adapter
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, workStatus);
+        // Connect adapter to spinner
+        sp.setAdapter(dataAdapter);
+    }
+}
