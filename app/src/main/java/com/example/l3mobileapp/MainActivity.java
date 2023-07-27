b12d16387cfeb32cf50030438700673108a67819
PhanTien234 <minhtienp328@gmail.com>
2023-07-27 23:16:27 +0700

    Create Alert Dialog

--- app/src/main/java/com/example/l3mobileapp/MainActivity.java
+++ app/src/main/java/com/example/l3mobileapp/MainActivity.java
@@ -5,12 +5,16 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -35,33 +39,72 @@ public class MainActivity extends AppCompatActivity {
         }
     }
     // Create an array with options
-    private String [] workStatus = {"Employed", "Unemployed"};
-    Spinner sp;
+/*    private String [] workStatus = {"Employed", "Unemployed"};*/
     public void updateDOB(LocalDate dob){
         TextView dobControl = findViewById(R.id.dob_control);
         dobControl.setText(dob.toString());
     }
+    //Define variables to references the layout
+    EditText nameInput;
+    EditText emailInput;
+    EditText phoneInput;
+    Spinner sp;
+    String workStatus;
+    Button submitBtn;
+
+    //Define the function getInputs() to get values from inputs
+    private void getInputs(){
+        nameInput = findViewById(R.id.name_input);
+        emailInput = findViewById(R.id.email_input);
+        phoneInput = findViewById(R.id.phone_input);
+
+        String name = nameInput.getText().toString();
+        String email = emailInput.getText().toString();
+        String phone = phoneInput.getText().toString();
+        workStatus = sp.getSelectedItem().toString();
+
+        //Use function displayNextALert to display an AlertDialog
+        displayNextAlert(name, phone, email, workStatus);
+    }
+    public void  displayNextAlert(String name, String phone, String email, String workStatus){
+        new AlertDialog.Builder(this)
+                .setTitle("Detail Entered")
+                .setMessage(
+                        "Details: \n" +
+                                name + "\n" +
+                                phone + "\n" +
+                                email + "\n" +
+                                workStatus
+                        )
+                .setNeutralButton("Back", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                })
+                .show();
+    }
+
     TextView dobControl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        dobControl = findViewById(R.id.dob_control);
-        dobControl.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                DialogFragment newFragment = new DatePickerFragment();
-                newFragment.show(getSupportFragmentManager(), "datePicker");
-            }
-        });
-        // Get reference to spinner
+        // ...
+
+        // Refer the Spinner from the layout
         sp = findViewById(R.id.spinner);
 
-        // Create an adapter
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_spinner_item, workStatus);
-        // Connect adapter to spinner
-        sp.setAdapter(dataAdapter);
+        // Refer the button from the layout
+        submitBtn = findViewById(R.id.submit_btn);
+
+        // Adding behavior to the button
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getInputs();
+            }
+        });
     }
 }
